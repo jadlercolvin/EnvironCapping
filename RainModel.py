705bc354ed9cfd5f85106ddc92313aeba3b1f8df
jadlercolvin <60368479+jadlercolvin@users.noreply.github.com>
2021-05-03 20:26:30 -0400

    Update RainModel.py

--- RainModel.py
+++ RainModel.py
@@ -1,4 +1,4 @@
 exRain=float(input("Enter expected rainfall in centimeters:"))
-time=(7.9685*10**10*exRain)/(2.567*10**8)
+time=(7.9685*10**10*exRain)/(2.567*10**10)
 result=time//3.12
 print(result)
